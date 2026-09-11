c609e6fe91861440d933c3ab004fff4d8b97fa37
thanatchana <thanatchana21@gmail.com>
2026-09-11 13:19:37 +0700

    v.2-Template-339

---
+++ thanatchana-project/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-form class="q-gutter-md" style="max-width: 600px">
+      <q-input filled label="Your name *" hint="Name and surname" />
+      <q-input filled type="number" label="Your age *" />
+      <q-toggle label="I accept the license and terms" />
+      <div>
+        <q-btn label="SUBMIT" type="submit" color="primary" />
+        <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
