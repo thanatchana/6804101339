697a1cbe4aeec548a1dcb71b4438e64324a90725
thanatchana <thanatchana21@gmail.com>
2026-09-11 13:20:32 +0700

    v.3-Script setup-339

--- thanatchana-project/src/pages/IndexPage.vue
+++ thanatchana-project/src/pages/IndexPage.vue
@@ -13,5 +13,31 @@
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+const name = ref('')
+const age = ref(null)
+const accept = ref(false)
+
+function onSubmit () {
+  if (accept.value !== true) {
+    $q.notify({
+      type: 'negative',
+      message: 'You need to accept the license and terms first'
+    })
+  } else {
+    $q.notify({
+      type: 'positive',
+      message: 'Submitted successfully'
+    })
+  }
+}
+
+function onReset () {
+  name.value = ''
+  age.value = null
+  accept.value = false
+}
 </script>
