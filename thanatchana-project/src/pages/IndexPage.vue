57cdc88645f921d64cc231da8e7e6129c31ec0c3
thanatchana <thanatchana21@gmail.com>
2026-09-11 13:36:06 +0700

    v.5-Accept with Chinese language-339

--- thanatchana-project/src/pages/IndexPage.vue
+++ thanatchana-project/src/pages/IndexPage.vue
@@ -3,7 +3,7 @@
     <q-form class="q-gutter-md" style="max-width: 600px">
       <q-input filled label="Your name *" hint="Name and surname" />
       <q-input filled type="number" label="Your age *" />
-      <q-toggle label="I accept the license and terms" />
+      <q-toggle label="I accept the license and terms (我接受许可和条款语言)" />
       <div>
         <q-btn label="SUBMIT" type="submit" color="primary" />
         <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
