dbaaf00244f8f97cc137475b2a8485fba7124259
thanatchana <thanatchana21@gmail.com>
2026-09-11 14:27:10 +0700

    master339

--- thanatchana-project/src/pages/IndexPage.vue
+++ thanatchana-project/src/pages/IndexPage.vue
@@ -3,10 +3,6 @@
     <q-form class="q-gutter-md" style="max-width: 600px">
       <q-input filled label="Your name (ชื่อ-สกุล) *" hint="Name and surname" />
       <q-input filled type="number" label="Your age (อายุ) *" />
-      <q-toggle label="I accept the license and terms" />
-
-      <q-input filled label="Your name *" hint="Name and surname" />
-      <q-input filled type="number" label="Your age *" />
       <q-toggle label="I accept the license and terms (我接受许可和条款语言)" />
       <div>
         <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
